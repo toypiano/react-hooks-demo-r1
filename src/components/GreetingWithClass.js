a6d7f24ab60d6e1e870a54980d40216ecbd89cb2
Hayoun Lee <sidhlee@gmail.com>
2020-02-02 20:42:47 +0900

    Add width display and resize listener + cWU cleanup

--- src/components/GreetingWithClass.js
+++ src/components/GreetingWithClass.js
@@ -5,23 +5,35 @@ import { ThemeContext, LocaleContext } from "./context";
 export default class Greeting extends React.Component {
   state = {
     name: "Toy",
-    surname: "Piano"
+    surname: "Piano",
+    width: window.innerWidth
   };
 
   componentDidMount() {
     document.title = this.state.name + " " + this.state.surname;
+    window.addEventListener("resize", this.handleResize);
   }
 
   componentDidUpdate() {
     document.title = this.state.name + " " + this.state.surname;
   }
 
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.handleResize);
+  }
+
   handleNameChange = e => {
     this.setState({ name: e.target.value });
   };
+
   handleSurnameChange = e => {
     this.setState({ surname: e.target.value });
   };
+
+  handleResize = e => {
+    this.setState({ width: window.innerWidth });
+  };
+
   render() {
     return (
       <ThemeContext.Consumer>
@@ -42,6 +54,7 @@ export default class Greeting extends React.Component {
             <LocaleContext.Consumer>
               {locale => <Row label="From">{locale}</Row>}
             </LocaleContext.Consumer>
+            <Row label="Width">{this.state.width}</Row>
           </section>
         )}
       </ThemeContext.Consumer>
