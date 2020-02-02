6779662c6bece62acec67f03b2930f0c750f16ad
Hayoun Lee <sidhlee@gmail.com>
2020-02-02 17:42:21 +0900

    Start Greeting class component

--- src/components/GreetingWithClass.js
+++ src/components/GreetingWithClass.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Row from "./Row";
 
 export default class Greeting extends React.Component {
+  state = {
+    name: "Paul"
+  };
   render() {
-    return <section>Greeting</section>;
+    return (
+      <section>
+        <Row label="Name">{this.state.name}</Row>
+      </section>
+    );
   }
 }
