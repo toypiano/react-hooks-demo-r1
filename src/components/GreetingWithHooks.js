5da10fade059652912e41a539b85a6304052be2b
Hayoun Lee <sidhlee@gmail.com>
2020-02-02 21:04:46 +0900

    Add useState for surname

--- src/components/GreetingWithHooks.js
+++ src/components/GreetingWithHooks.js
@@ -3,14 +3,21 @@ import Row from "./Row";
 
 export default function Greeting(props) {
   const [name, setName] = useState("Real");
+  const [surname, setSurname] = useState("B");
 
   const handleNameChange = e => {
     setName(e.target.value);
+  };
+  const handleSurnameChange = e => {
+    setSurname(e.target.value);
   };
   return (
     <section>
       <Row label="Name">
         <input value={name} onChange={handleNameChange} />
+      </Row>
+      <Row label="Surname">
+        <input value={surname} onChange={handleSurnameChange} />
       </Row>
     </section>
   );
