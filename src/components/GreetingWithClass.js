003b007108ccaf1342a58b4aec304cb15a80b5c7
Hayoun Lee <sidhlee@gmail.com>
2020-02-02 18:41:57 +0900

    Add styles for Row component

--- src/components/GreetingWithClass.js
+++ src/components/GreetingWithClass.js
@@ -3,11 +3,15 @@ import Row from "./Row";
 
 export default class Greeting extends React.Component {
   state = {
-    name: "Paul"
+    name: "Paul",
+    surname: "Gilbert"
   };
 
   handleNameChange = e => {
     this.setState({ name: e.target.value });
+  };
+  handleSurnameChange = e => {
+    this.setState({ surname: e.target.value });
   };
   render() {
     return (
@@ -16,6 +20,12 @@ export default class Greeting extends React.Component {
           <input
             value={this.state.name}
             onChange={this.handleNameChange}
+          />
+        </Row>
+        <Row label="Surname">
+          <input
+            value={this.state.surname}
+            onChange={this.handleSurnameChange}
           />
         </Row>
       </section>
